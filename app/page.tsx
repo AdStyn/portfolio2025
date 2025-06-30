bc3685d5093e1dbc7b3d8f7382d40a732868e75b
AdStyn <kancell124@gmail.com>
2025-07-01 01:14:18 +0700

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,79 @@
-// "use client";
+"use client";
 import RotatingText from "./components/RotatingText/RotatingText";
 import Lanyard from "./components/Lanyard/Lanyard";
 import SplashCursor from "./components/SplashCursor/SplashCursor";
+import TextPressure from "./components/TextPressure/TextPressure";
+import AnimatedContent from "./components/AnimatedContent/AnimatedContent";
+import BlurText from "./components/BlurText/BlurText";
+import ScrollVelocity from "./components/ScrollVelocity/ScrollVelocity";
+import GooeyNav from "./components/GooeyNav/GooeyNav";
 
 export default function Home() {
+  const nav = [
+    { label: "Beranda", href: "#" },
+    { label: "Tentang", href: "#" },
+    { label: "Kontak", href: "#" },
+  ];
   return (
-    <div className="min-h-screen overflow-x-hidden bg-black">
+    <div className="flex flex-col bg-black overflow-x-hidden">
+      <div className=" fixed top-4 left-1/2 transform -translate-x-1/2 z-50 flex items-center justify-center h-[100px]">
+        <GooeyNav
+          items={nav}
+          particleCount={15}
+          particleDistances={[90, 10]}
+          particleR={100}
+          initialActiveIndex={0}
+          animationTime={600}
+          timeVariance={300}
+          colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+        ></GooeyNav>
+      </div>
+
       <SplashCursor />
-      <div className="container mx-auto h-screen">
+
+      {/* Main content with flex-grow */}
+
+      <div className="container mx-auto min-h-screen">
         <div className="grid grid-cols-12 gap-4 justify-between items-center h-full">
           <div className="col-span-6">
-            <div className="flex flex-col h-full items-start justify-center">
-              <h1 className="text-3xl font-bold">Hallo I'am</h1>
-              <div className="flex flex-row items-center gap-4 mt-2 mb-4">
-                <h1 className="text-5xl font-bold">Adstyn</h1>
+            <AnimatedContent
+              distance={150}
+              direction="horizontal"
+              reverse={false}
+              duration={1.2}
+              ease="bounce.out"
+              initialOpacity={0.2}
+              animateOpacity
+              scale={1.1}
+              threshold={0.2}
+              delay={0.5}
+            >
+              <h1 className="text-7xl font-bold text-white">HALLO I 'M</h1>
+
+              <div className="flex flex-row items-center gap-6 mt-2 mb-4-4">
+                <div className="w-[300px] h-[100px]">
+                  <TextPressure
+                    text="ADSTYN"
+                    flex={true}
+                    alpha={false}
+                    stroke={false}
+                    width={true}
+                    weight={true}
+                    italic={true}
+                    textColor="#ffffff"
+                    strokeColor="#ff0000"
+                    minFontSize={5}
+                    className="text-xl"
+                  />
+                </div>
                 <RotatingText
                   texts={[
-                    "FrontEnd Development",
                     "UI/UX Design",
+                    "FrontEnd Development",
                     "Web Designer",
+                    "IT Project Manager",
                   ]}
-                  mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-[#191919] overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg text-center text-2xl sm:text-3xl md:text-4xl font-bold inline-flex"
+                  mainClassName="px-2 sm:px-2 md:px-3 text-cyan-300 py-2 sm:py-1 md:py-2  rounded-lg text-center text-xl sm:text-3xl md:text-4xl font-bold inline-flex"
                   staggerFrom={"last"}
                   initial={{ y: "100%" }}
                   animate={{ y: 0 }}
@@ -31,15 +84,36 @@ export default function Home() {
                   rotationInterval={3000}
                 />
               </div>
-              <p className="text-lg text-gray-400">
-                Vocational high school graduate majoring in Software Engineering
-                who has a great interest in the field of programming and
-                software development. Has experience in creating various web and
-                mobile application projects. Ready to actively contribute in the
-                world of work with adequate technical and soft skills.
-              </p>
-            </div>
+              <BlurText
+                text="Saya adalah Maba Bisnis Digital yang bekerja secara hybrid sebagai Frontend Developer dan UI/UX Designer, dengan pengalaman kurang lebih 3 tahun dalam membangun antarmuka web yang responsif dan user-friendly."
+                delay={10}
+                animateBy="letters"
+                direction="top"
+                className="text-lg flex mt-4 text-center text-gray-500 h-full hover:text-gray-300 transition-colors"
+              />
+              <div className="gap-4 mt-6 flex-row">
+                <button className="bg-cyan-300 mr-6 text-black text-xl px-4 py-2 rounded-lg text-lg font-semibold transition-colors duration-300">
+                  <a
+                    href="https://drive.google.com/file/d/17ms6mHNKZ5AnbdpEkEoVZKcht_pUY9O2/view"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Download CV
+                  </a>
+                </button>
+                <button className="bg-gray-300 gap-4 text-black text-xl px-4 py-2 rounded-lg text-lg font-semibold transition-colors duration-300">
+                  <a
+                    href="https://drive.google.com/drive/u/0/folders/1cfUmDp-1TXFITd6n2uooq1jj4qANmLGC"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Certificates
+                  </a>
+                </button>
+              </div>
+            </AnimatedContent>
           </div>
+
           <div className="col-span-6">
             <div className="flex flex-col items-center justify-center h-full">
               <div className="w-full h-full flex items-center justify-center">
@@ -48,7 +122,24 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        {/* About section */}
+        <BlurText
+          text="Tentang Saya"
+          delay={50}
+          animateBy="words"
+          direction="top"
+          className="text-4xl mb-8 text-center justify-center text-cyan-300 font-bold"
+        />
       </div>
+
+      {/* Footer always at the bottom */}
+      <footer className="h-24 text-center justify-center mt-50">
+        <ScrollVelocity
+          texts={["Portfolio Adstyn", "All rights reserved ©2025"]}
+          className="text-center custom-scroll-text text-5xl justify-center "
+        />
+      </footer>
     </div>
   );
 }
